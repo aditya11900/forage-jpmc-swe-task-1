a5f4364d9816dffb5c374dacd0e671770a6a5963
aditya11900 <adityaraj153566@gmail.com>
2024-04-13 15:02:10 +0530

    Fixed the broken client datafeed script

--- client3.py
+++ client3.py
@@ -1,3 +1,7 @@
+import json
+import random
+import datetime
+
 ################################################################################
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a
@@ -18,8 +22,6 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
-import json
-import random
 import urllib.request
 
 # Server API URLs
@@ -28,32 +30,58 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
-
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
-    stock = quote['stock']
-    bid_price = float(quote['top_bid']['price'])
-    ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    if isinstance(quote, dict):
+        timestamp_str = quote['timestamp']
+        stock = quote['stock']
+        price = quote['price']
+        bid_price = quote['top_bid']['price']
+        ask_price = quote['top_ask']['price']
+    else:
+        _, _, _, bid_price_str, ask_price_str = quote.split(',')
+        bid_price = float(bid_price_str)
+        ask_price = float(ask_price_str)
+
+    # Parsing the timestamp string
+    try:
+        timestamp = datetime.datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S.%f')
+    except ValueError:
+        raise ValueError("Invalid timestamp format")
+
+    # Extracting the price
+    try:
+        price = float(price)
+    except ValueError:
+        raise ValueError("Invalid price format")
+
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None # Avoid zero division error
+    return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+        try:
+            quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
-        for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            """ ----------- Update to get the ratio --------------- """
+            stock_a, bid_price_a, ask_price_a, price_a = getDataPoint(quotes[0])
+            stock_b, bid_price_b, ask_price_b, price_b = getDataPoint(quotes[1])
 
-        print("Ratio %s" % getRatio(price, price))
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock_a, bid_price_a, ask_price_a, price_a))
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock_b, bid_price_b, ask_price_b, price_b))
+
+            # Calculate the ratio
+            ratio = getRatio(price_a, price_b)
+            print("Ratio %s" % ratio)
+        except Exception as e:
+            print("Message: %s" % e)
--- client_test.py
+++ client_test.py
@@ -2,24 +2,31 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC', 'price': 120.84},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF', 'price': 119.775}
+        ]
+        """ ------------ Add the assertion below ------------ """
+        for i, quote in enumerate(quotes):
+            with self.subTest(i=i):
+                _, bid_price, ask_price, price = getDataPoint(quote)
+                # Ensure the calculated price is correct
+                self.assertAlmostEqual(price, (bid_price + ask_price) / 2, places=2)
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
-
-
-  """ ------------ Add more unit tests ------------ """
-
-
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC', 'price': 119.84},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF', 'price': 119.775}
+        ]
+        
+        for i, quote in enumerate(quotes):
+            with self.subTest(i=i):
+                _, bid_price, ask_price, price = getDataPoint(quote)
+                # Ensure the calculated price is correct
+                self.assertAlmostEqual(price, (bid_price + ask_price) / 2, places=2)
+                # Ensure the bid price is greater than the ask price
+               
 
 if __name__ == '__main__':
     unittest.main()
